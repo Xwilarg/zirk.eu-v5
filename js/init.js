da92e13682b902f74ec608527a88d6d03eead8b7
Xwilarg <xwilarg@yahoo.fr>
2022-02-16 02:34:37 +0100

    Improve accessibility

--- js/init.js
+++ js/init.js
@@ -1,6 +1,10 @@
 window.onload = function() {
     const urlParams = new URLSearchParams(window.location.search);
     const tab = urlParams.get('tab');
+
+    for (elem of document.getElementsByClassName("globalTab")) {
+        elem.hidden = true;
+    }
     if (tab !== null) {
         displayTab('globalTab', tab.toLowerCase());
     }
