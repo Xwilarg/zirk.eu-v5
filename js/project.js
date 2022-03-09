4696325765e6e4252ed48e64c285530a217b15f8
Xwilarg <xwilarg@yahoo.fr>
2022-03-09 22:16:17 +0100

    Add more projects

--- js/project.js
+++ js/project.js
@@ -19,7 +19,9 @@ let projects = [
                 "content": "https://discordapp.com/oauth2/authorize?client_id=329664361016721408&permissions=3196928&scope=bot"
             }
         ],
-        "nsfw": false
+        "nsfw": false,
+        "type": "Discord Bot",
+        "highlist": true
     },
     {
         "name": "NHentaiDownloader",
@@ -37,7 +39,9 @@ let projects = [
                 "content": "https://nhentaidownloader.zirk.eu"
             }
         ],
-        "nsfw": true
+        "nsfw": true,
+        "type": "Web Extension",
+        "highlist": true
     },
     {
         "name": "BooruSharp",
@@ -55,7 +59,9 @@ let projects = [
                 "content": "https://www.nuget.org/packages/BooruSharp"
             }
         ],
-        "nsfw": true
+        "nsfw": true,
+        "type": "Library",
+        "highlist": true
     },
     {
         "name": "Discord OAuth2 PHP",
@@ -73,7 +79,9 @@ let projects = [
                 "content": "https://packagist.org/packages/xwilarg/discord-oauth2-php"
             }
         ],
-        "nsfw": false
+        "nsfw": false,
+        "type": "Library",
+        "highlist": false
     },
     {
         "name": "DailyReset",
@@ -87,7 +95,9 @@ let projects = [
                 "content": "https://github.com/Xwilarg/DailyReset"
             }
         ],
-        "nsfw": false
+        "nsfw": false,
+        "Android App": "Library",
+        "highlist": false
     },
     {
         "name": "Euphonia",
@@ -101,7 +111,137 @@ let projects = [
                 "content": "https://github.com/Xwilarg/Euphonia"
             }
         ],
-        "nsfw": false
+        "nsfw": false,
+        "type": "Website",
+        "highlist": true
+    },
+    {
+        "name": "Colodex",
+        "id": "Colodex",
+        "description": "Library to use Holodex in C",
+        "imageDescription": "Sample code that display the name and organization associated with a YouTube channel ID",
+        "languages": [ "C" ],
+        "links": [
+            {
+                "name": "Source Code",
+                "content": "https://github.com/Xwilarg/Colodex"
+            }
+        ],
+        "nsfw": false,
+        "type": "Library",
+        "highlist": false
+    },
+    {
+        "name": "GitHub Traffic",
+        "id": "GitHubTraffic",
+        "description": "Statistics about the traffic of your GitHub repositories",
+        "imageDescription": "Latest traffic applied on account \"Xwilarg\"",
+        "languages": [ "Python" ],
+        "links": [
+            {
+                "name": "Source Code",
+                "content": "https://github.com/Xwilarg/GitHubTraffic"
+            }
+        ],
+        "nsfw": false,
+        "type": "Library",
+        "highlist": false
+    },
+    {
+        "name": "FPL-TimePlayed",
+        "id": "FPL-TimePlayed",
+        "description": "Flashpoint extension to display how much time you spent on a game",
+        "imageDescription": "Upper part: extension settings, lower part: display example",
+        "languages": [ "Typescript" ],
+        "links": [
+            {
+                "name": "Source Code",
+                "content": "https://github.com/Xwilarg/FPL-TimePlayed"
+            }
+        ],
+        "nsfw": false,
+        "type": "Flashpoint extension",
+        "highlist": false
+    },
+    {
+        "name": "TwitterSharp",
+        "id": "TwitterSharp",
+        "description": "C# wrapper around Twitter API V2",
+        "imageDescription": "Code to search the 10 latests tweet of Mori Calliope ",
+        "languages": [ "C#" ],
+        "links": [
+            {
+                "name": "Source Code",
+                "content": "https://github.com/Xwilarg/TwitterSharp"
+            }
+        ],
+        "nsfw": false,
+        "type": "Library",
+        "highlist": false
+    },
+    {
+        "name": "Pina",
+        "id": "Pina",
+        "description": "A Discord bot that allow anyone to pin messages in your server",
+        "imageDescription": "Example of pinnig a message",
+        "languages": [ "C#" ],
+        "links": [
+            {
+                "name": "Source Code",
+                "content": "https://github.com/Xwilarg/Pina"
+            }
+        ],
+        "nsfw": false,
+        "type": "Discord Bot",
+        "highlist": false
+    },
+    {
+        "name": "CSV Cleaner",
+        "id": "CSVCleaner",
+        "description": "Some handy tools to clean your CSV files",
+        "imageDescription": "CSV data loaded into the software",
+        "languages": [ "C++" ],
+        "links": [
+            {
+                "name": "Source Code",
+                "content": "https://github.com/Xwilarg/CSVCleaner"
+            }
+        ],
+        "nsfw": false,
+        "type": "Software",
+        "highlist": false
+    },
+    {
+        "name": "Konawall",
+        "id": "Konawall",
+        "description": "Set a random anime wallpaper on your computer",
+        "imageDescription": "Script launched, searching for a wallpaper for the tag \"kantai_collection\"",
+        "languages": [ "Python" ],
+        "links": [
+            {
+                "name": "Source Code",
+                "content": "https://github.com/Xwilarg/Konawall"
+            }
+        ],
+        "nsfw": false,
+        "type": "Script",
+        "highlist": false
+    },
+    {
+        "name": "ProjetISNProcessing",
+        "id": "ProjetISNProcessing",
+        "description": "My first \"big\" video game project back in highschool",
+        "imageDescription": "Ongoing game",
+        "languages": [ "Java" ],
+        "links": [
+            {
+                "name": "Source Code",
+                "content": "https://github.com/Xwilarg/ProjetISNProcessing"
+            }
+        ],
+        "nsfw": false,
+        "type": "Video Game",
+        "highlist": false
     }
 ];
 
@@ -109,6 +249,14 @@ function displayProject(index) {
     document.getElementById("projectDisplay").hidden = false;
 
     let project = projects[index];
+    if (!project.nsfw)
+    {
+        document.getElementById("projectDisplayTitle").innerHTML = project.name;
+    }
+    else
+    {
+        document.getElementById("projectDisplayTitle").innerHTML = "";
+    }
     document.getElementById("projectDisplayText").innerHTML = project.description;
     document.getElementById("projectDisplayContent").src = `img/project/${project.id}.png`;
     document.getElementById("projectDisplaySubtext").innerHTML = project.imageDescription;
