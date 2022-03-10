20befda11601e6863ef4ba363588a6f29f85fa1c
Xwilarg <xwilarg@yahoo.fr>
2022-03-10 02:11:42 +0100

    Update projects

--- js/project.js
+++ js/project.js
@@ -1,8 +1,165 @@
 let projects = [
     {
+        "name": "Euphonia",
+        "id": "Euphonia",
+        "description": "Website to host and play your music",
+        "comment": "I was tired of others music platforms so I made my own!",
+        "imageDescription": "Example of the website with a music playing. Top right: Preview from mobile notification",
+        "languages": [ "JavaScript", "PHP", "Python" ],
+        "links": [
+            {
+                "name": "Source Code",
+                "content": "https://github.com/Xwilarg/Euphonia"
+            }
+        ],
+        "nsfw": false,
+        "type": "Website",
+        "highlist": true,
+        "date": "Since 2021-11-25"
+    },
+    {
+        "name": "Colodex",
+        "id": "Colodex",
+        "description": "Library to use Holodex (vtuber stream aggregator) in C",
+        "comment": "",
+        "imageDescription": "Sample code that display the name and organization associated with a YouTube channel ID",
+        "languages": [ "C" ],
+        "links": [
+            {
+                "name": "Source Code",
+                "content": "https://github.com/Xwilarg/Colodex"
+            }
+        ],
+        "nsfw": false,
+        "type": "Library",
+        "highlist": false,
+        "date": "Since 2021-09-08"
+    },
+    {
+        "name": "TwitterSharp",
+        "id": "TwitterSharp",
+        "description": "C# wrapper around Twitter API V2",
+        "comment": "This project is referenced by Twitter Developer Platform! https://developer.twitter.com/en/docs/twitter-api/tools-and-libraries/v2",
+        "imageDescription": "Code to search the 10 latests tweet of Mori Calliope ",
+        "languages": [ "C#" ],
+        "links": [
+            {
+                "name": "Source Code",
+                "content": "https://github.com/Xwilarg/TwitterSharp"
+            }
+        ],
+        "nsfw": false,
+        "type": "Library",
+        "highlist": false,
+        "date": "Since 2021-05-03"
+    },
+    {
+        "name": "GitHub Traffic",
+        "id": "GitHubTraffic",
+        "description": "Statistics about the traffic of your GitHub repositories",
+        "comment": "",
+        "imageDescription": "Latest traffic applied on account \"Xwilarg\"",
+        "languages": [ "Python" ],
+        "links": [
+            {
+                "name": "Source Code",
+                "content": "https://github.com/Xwilarg/GitHubTraffic"
+            }
+        ],
+        "nsfw": false,
+        "type": "Library",
+        "highlist": false,
+        "date": "Since 2021-04-10"
+    },
+    {
+        "name": "Pina",
+        "id": "Pina",
+        "description": "A Discord bot that allow anyone to pin messages in your server without the \"Manage Messages\" permission",
+        "comment": "Surely didn't expect this project to be much popular but it's doing decently I guess, still have weird bugs I have no idea how to solve tho",
+        "imageDescription": "Example of pinnig a message",
+        "languages": [ "C#" ],
+        "links": [
+            {
+                "name": "Source Code",
+                "content": "https://github.com/Xwilarg/Pina"
+            }
+        ],
+        "nsfw": false,
+        "type": "Discord Bot",
+        "highlist": false,
+        "date": "Since 2019-05-29"
+    },
+    {
+        "name": "Discord OAuth2 PHP",
+        "id": "Discord-OAuth2-PHP",
+        "description": "PHP library to easily use Discord OAuth2 authentification",
+        "comment": "",
+        "imageDescription": "Code to authentificate an user with Discord and get his username and Discord tag",
+        "languages": [ "PHP" ],
+        "links": [
+            {
+                "name": "Source Code",
+                "content": "https://github.com/Xwilarg/Discord-OAuth2-PHP"
+            },
+            {
+                "name": "Packagist",
+                "content": "https://packagist.org/packages/xwilarg/discord-oauth2-php"
+            }
+        ],
+        "nsfw": false,
+        "type": "Library",
+        "highlist": false,
+        "date": "Since 2019-04-19"
+    },
+    {
+        "name": "NHentaiDownloader",
+        "id": "NHentaiDownloader",
+        "description": "Allows to download doujinshi (fan-made manga) from some popular website",
+        "comment": "",
+        "imageDescription": "Download of many doujinshi at once",
+        "languages": [ "TypeScript" ],
+        "links": [
+            {
+                "name": "Source Code",
+                "content": "https://github.com/Xwilarg/NHentaiDownloader"
+            },
+            {
+                "name": "Website",
+                "content": "https://nhentaidownloader.zirk.eu"
+            }
+        ],
+        "nsfw": true,
+        "type": "Web Extension",
+        "highlist": true,
+        "date": "Since 2018-09-04"
+    },
+    {
+        "name": "BooruSharp",
+        "id": "BooruSharp",
+        "description": "C# library to download images from Booru websites (anime image aggregator)",
+        "comment": "Making a common library for so many websites is such a pain, I still don't know why my unit tests randomly fails",
+        "imageDescription": "Code to get the URL and rating of a random image having the \"Cirno\" tag",
+        "languages": [ "C#" ],
+        "links": [
+            {
+                "name": "Source Code",
+                "content": "https://github.com/Xwilarg/BooruSharp"
+            },
+            {
+                "name": "NuGet",
+                "content": "https://www.nuget.org/packages/BooruSharp"
+            }
+        ],
+        "nsfw": true,
+        "type": "Library",
+        "highlist": true,
+        "date": "Since 2018-07-16"
+    },
+    {
         "name": "Sanara",
         "id": "Sanara",
         "description": "Multipurpose bot for Discord",
+        "comment": "Probably my favorite project so far!",
         "imageDescription": "Top left: image search<br/>Bottom left: shiritori (Japanese associative word game)<br/>Right: text translation from image",
         "languages": [ "C#" ],
         "links": [
@@ -25,140 +182,10 @@ let projects = [
         "date": "Since 2017-08-28"
     },
     {
-        "name": "NHentaiDownloader",
-        "id": "NHentaiDownloader",
-        "description": "Allows to download doujinshi (fan-made manga) from some popular website",
-        "imageDescription": "Download of many doujinshi at once",
-        "languages": [ "TypeScript" ],
-        "links": [
-            {
-                "name": "Source Code",
-                "content": "https://github.com/Xwilarg/NHentaiDownloader"
-            },
-            {
-                "name": "Website",
-                "content": "https://nhentaidownloader.zirk.eu"
-            }
-        ],
-        "nsfw": true,
-        "type": "Web Extension",
-        "highlist": true,
-        "date": "Since 2018-09-04"
-    },
-    {
-        "name": "BooruSharp",
-        "id": "BooruSharp",
-        "description": "C# library to download images from Booru websites (anime image aggregator)",
-        "imageDescription": "Code to get the URL and rating of a random image having the \"Cirno\" tag",
-        "languages": [ "C#" ],
-        "links": [
-            {
-                "name": "Source Code",
-                "content": "https://github.com/Xwilarg/BooruSharp"
-            },
-            {
-                "name": "NuGet",
-                "content": "https://www.nuget.org/packages/BooruSharp"
-            }
-        ],
-        "nsfw": true,
-        "type": "Library",
-        "highlist": true,
-        "date": "Since 2018-07-16"
-    },
-    {
-        "name": "Discord OAuth2 PHP",
-        "id": "Discord-OAuth2-PHP",
-        "description": "PHP library to easily use Discord OAuth2 authentification",
-        "imageDescription": "Code to authentificate an user with Discord and get his username and Discord tag",
-        "languages": [ "PHP" ],
-        "links": [
-            {
-                "name": "Source Code",
-                "content": "https://github.com/Xwilarg/Discord-OAuth2-PHP"
-            },
-            {
-                "name": "Packagist",
-                "content": "https://packagist.org/packages/xwilarg/discord-oauth2-php"
-            }
-        ],
-        "nsfw": false,
-        "type": "Library",
-        "highlist": false,
-        "date": "Since 2019-04-19"
-    },
-    {
-        "name": "DailyReset",
-        "id": "DailyReset",
-        "description": "Android application to help learning Korean and Japanese",
-        "imageDescription": "Image 1: Daily word, Image 2: Quizz with handwritten input, Image 3: Quizz result, Image 4: Sample sentence with daily word",
-        "languages": [ "Kotlin" ],
-        "links": [
-            {
-                "name": "Source Code",
-                "content": "https://github.com/Xwilarg/DailyReset"
-            }
-        ],
-        "nsfw": false,
-        "Android App": "Library",
-        "highlist": false,
-        "date": "Since 2021-02-04 - 2021-12-20"
-    },
-    {
-        "name": "Euphonia",
-        "id": "Euphonia",
-        "description": "Website to host and play your music",
-        "imageDescription": "Example of the website with a music playing. Top right: Preview from mobile notification",
-        "languages": [ "JavaScript", "PHP", "Python" ],
-        "links": [
-            {
-                "name": "Source Code",
-                "content": "https://github.com/Xwilarg/Euphonia"
-            }
-        ],
-        "nsfw": false,
-        "type": "Website",
-        "highlist": true,
-        "date": "Since 2021-11-25"
-    },
-    {
-        "name": "Colodex",
-        "id": "Colodex",
-        "description": "Library to use Holodex in C",
-        "imageDescription": "Sample code that display the name and organization associated with a YouTube channel ID",
-        "languages": [ "C" ],
-        "links": [
-            {
-                "name": "Source Code",
-                "content": "https://github.com/Xwilarg/Colodex"
-            }
-        ],
-        "nsfw": false,
-        "type": "Library",
-        "highlist": false,
-        "date": "Since 2021-09-08"
-    },
-    {
-        "name": "GitHub Traffic",
-        "id": "GitHubTraffic",
-        "description": "Statistics about the traffic of your GitHub repositories",
-        "imageDescription": "Latest traffic applied on account \"Xwilarg\"",
-        "languages": [ "Python" ],
-        "links": [
-            {
-                "name": "Source Code",
-                "content": "https://github.com/Xwilarg/GitHubTraffic"
-            }
-        ],
-        "nsfw": false,
-        "type": "Library",
-        "highlist": false,
-        "date": "Since 2021-04-10"
-    },
-    {
         "name": "FPL-TimePlayed",
         "id": "FPL-TimePlayed",
         "description": "Flashpoint extension to display how much time you spent on a game",
+        "comment": "",
         "imageDescription": "Upper part: extension settings, lower part: display example",
         "languages": [ "Typescript" ],
         "links": [
@@ -173,43 +200,64 @@ let projects = [
         "date": "2021-04-04 - 2021-04-30"
     },
     {
-        "name": "TwitterSharp",
-        "id": "TwitterSharp",
-        "description": "C# wrapper around Twitter API V2",
-        "imageDescription": "Code to search the 10 latests tweet of Mori Calliope ",
-        "languages": [ "C#" ],
+        "name": "DailyReset",
+        "id": "DailyReset",
+        "description": "Android application to help learning Korean and Japanese, give a new word every day and do quizzes based on them",
+        "comment": "Made this project to motivate myself to continue learning Japanese... that didn't work",
+        "imageDescription": "Image 1: Daily word<br/>Image 2: Quizz with handwritten input<br/>Image 3: Quizz result<br/>Image 4: Sample sentence with daily word",
+        "languages": [ "Kotlin" ],
         "links": [
             {
                 "name": "Source Code",
-                "content": "https://github.com/Xwilarg/TwitterSharp"
+                "content": "https://github.com/Xwilarg/DailyReset"
             }
         ],
         "nsfw": false,
-        "type": "Library",
+        "type": "Android App",
         "highlist": false,
-        "date": "Since 2021-05-03"
+        "date": "Since 2021-02-04 - 2021-12-20"
     },
     {
-        "name": "Pina",
-        "id": "Pina",
-        "description": "A Discord bot that allow anyone to pin messages in your server",
-        "imageDescription": "Example of pinnig a message",
+        "name": "Yuuka",
+        "id": "Yuuka",
+        "description": "A Discord bot that allow you to create tags with text, audio or images, and then play them when you want to",
+        "comment": "",
+        "imageDescription": "Tags",
         "languages": [ "C#" ],
         "links": [
             {
                 "name": "Source Code",
-                "content": "https://github.com/Xwilarg/Pina"
+                "content": "https://github.com/Xwilarg/Yuuka"
             }
         ],
         "nsfw": false,
         "type": "Discord Bot",
         "highlist": false,
-        "date": "Since 2019-05-29"
+        "date": "2020-07-20 - 2021-12-21"
+    },
+    {
+        "name": "Natsuri",
+        "id": "Natsuri",
+        "description": "A Discord bot than open a gate allowing you to send an image to a random guild and to see the reactions they add to it",
+        "comment": "I was expecting this project to be more popular but it ended up being a lot of work for not that many people using it",
+        "imageDescription": "Generation of text based on last messages the user sent",
+        "languages": [ "C#" ],
+        "links": [
+            {
+                "name": "Source Code",
+                "content": "https://github.com/Xwilarg/Natsuri"
+            }
+        ],
+        "nsfw": false,
+        "type": "Discord Bot",
+        "highlist": false,
+        "date": "2020-11-22 - 2020-12-04"
     },
     {
         "name": "CSV Cleaner",
         "id": "CSVCleaner",
         "description": "Some handy tools to clean your CSV files",
+        "comment": "A tool I made for someone who had to manually clean CSV files, I'm pretty happy of the end result",
         "imageDescription": "CSV data loaded into the software",
         "languages": [ "C++" ],
         "links": [
@@ -224,9 +272,28 @@ let projects = [
         "date": "2019-01-22 - 2019-02-03"
     },
     {
+        "name": "Lania",
+        "id": "Lania",
+        "description": "A Discord bot than open a gate allowing you to send an image to a random guild and to see the reactions they add to it",
+        "comment": "Was expecting this project to be more popular but it never took of",
+        "imageDescription": "Left: An image send to others guilds<br/>Right: 2 images received",
+        "languages": [ "C#" ],
+        "links": [
+            {
+                "name": "Source Code",
+                "content": "https://github.com/Xwilarg/Lania"
+            }
+        ],
+        "nsfw": false,
+        "type": "Discord Bot",
+        "highlist": false,
+        "date": "2018-06-09 - 2020-01-06"
+    },
+    {
         "name": "Konawall",
         "id": "Konawall",
         "description": "Set a random anime wallpaper on your computer",
+        "comment": "",
         "imageDescription": "Script launched, searching for a wallpaper for the tag \"kantai_collection\"",
         "languages": [ "Python" ],
         "links": [
@@ -241,26 +308,46 @@ let projects = [
         "date": "2018-06-08 - 2018-10-05"
     },
     {
+        "name": "Atsuka",
+        "id": "Atsuka",
+        "description": "Detect \"bad\" messages and prevent user to send messages until he apologize",
+        "comment": "This project nearly destroyed a school group project after someone became angry toward the bot (and me)",
+        "imageDescription": "Messages are automatically deleted until the user apologize",
+        "languages": [ "C#" ],
+        "links": [
+            {
+                "name": "Source Code",
+                "content": "https://github.com/Xwilarg/Atsuka"
+            }
+        ],
+        "nsfw": false,
+        "type": "Discord Bot",
+        "highlist": false,
+        "date": "2018-04-25 - 2019-07-14"
+    },
+    {
         "name": "FightEpitechGrades",
         "id": "FightEpitechGrades",
-        "description": "This project is a 2D video game made with the SFML where you fight your EPITECH's grades",
+        "description": "2D fighting game",
+        "comment": "First games I made at EPITECH, having people trying to beat the AI was a lot of fun",
         "imageDescription": "Player versus AI",
         "languages": [ "C++" ],
         "links": [
             {
                 "name": "Source Code",
-                "content": "https://github.com/Xwilarg/FightEpitechGrades"
+                "content": "https://github.com/Xwilarg/FightEpitechGrades-Old"
             }
         ],
         "nsfw": false,
         "type": "Video Game",
         "highlist": false,
-        "date": "2015 - 2016"
+        "date": "End of 2016"
     },
     {
         "name": "ProjetISNProcessing",
         "id": "ProjetISNProcessing",
-        "description": "My first \"big\" video game project back in highschool",
+        "description": "RPG",
+        "comment": "Final project of my last year of highschool, I kind of wanted to continue it but after some time the game become really laggy, never found out why",
         "imageDescription": "Ongoing game",
         "languages": [ "Java" ],
         "links": [
@@ -277,7 +364,8 @@ let projects = [
     {
         "name": "The Quest Of Zirk",
         "id": "TQOZ",
-        "description": "Game for TI-83+ I made around the middle of my high school years",
+        "description": "Text-based game for TI-83+",
+        "comment": "Game I made back in high-school on my calculator, had to stop because I reached the limit of 99 GOTO I could do",
         "imageDescription": "Screen 1: Main menu (Main menu 1-Play 2-Stats 3-Reset 4-Achievements 5-Codes 6-Credit 7-Exit)<br/>Screen 2: Fight (You are attacking, your enemy loose: 12  He Still has: 33)<br/>Screen 3: In-game menu (Where are you going? 1- Capital 2-City 3-River 4-Graveyard 5-Dungeon)<br/>Screen 4: Stats recap (Enemies killed: 1  Damages done: 67  Damages received: 100)",
         "languages": [ "TI-Basic" ],
         "links": [
